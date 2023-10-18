772a49e0b750604047d7344476e4fbd01e1dd5a7
yiir27 <camp.yui.c@gmail.com>
2023-10-19 03:10:22 +0900

    c099.jsOK

---
+++ c099.js
@@ -0,0 +1,14 @@
+const lines = ['3 4','2','1'];
+
+// const [ , area] = lines[0].split(' ').map(Number);
+// const widthArr = lines.slice(1).map(Number);
+const [ , area, ...widthArr] = lines.join(' ').split(' ').map(Number);
+const culcurate = (a, arr) => {
+    let sum = 0;
+    arr.forEach((num) => {
+        sum += (a - num);
+    })
+    return sum;
+}
+console.log(area*(area + culcurate(area, widthArr)));
+// console.log(widthArr);
